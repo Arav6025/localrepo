a2ea20ac29c48a495b372ccce98f9ebfc16f05ef
Arav6025 <btech10643.24@bitmesra.ac.in>
2025-08-10 22:52:06 +0530

    new_cpp_file

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class : public : void display()
+{
+    cout << "This is a test for local repository." << endl;
+    cout << "This file is used to test the local repository functionality in a web application." << endl;
+}
+int main()
+{
+
+    class obj;
+    obj.display();
+    return 0;
+}
--- work.java
+++ work.java
@@ -1,4 +1,4 @@
-public class work{
+public class work {
     public static void main(String[] args) {
         System.out.println("This is a test for local repository.");
         System.out.println("This file is used to test the local repository functionality in a web application.");
