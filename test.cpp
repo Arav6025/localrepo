ac6f1865e11b0a14282b3a96cb15fca356b650aa
Arav6025 <btech10643.24@bitmesra.ac.in>
2025-08-10 22:54:32 +0530

    correct_cpp_file

--- test.cpp
+++ test.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class : public : void display()
+int main()
 {
     cout << "This is a test for local repository." << endl;
     cout << "This file is used to test the local repository functionality in a web application." << endl;
-}
-int main()
-{
-
-    class obj;
-    obj.display();
     return 0;
 }
