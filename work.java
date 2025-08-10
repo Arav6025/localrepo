793121cd961ae69ed7938056fb3b5557f179484d
Arav6025 <btech10643.24@bitmesra.ac.in>
2025-08-10 22:44:14 +0530

    new_java_file

---
+++ work.java
@@ -0,0 +1,6 @@
+public class work{
+    public static void main(String[] args) {
+        System.out.println("This is a test for local repository.");
+        System.out.println("This file is used to test the local repository functionality in a web application.");
+    }
+}
